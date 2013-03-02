3aedfff762a4a94297f0d496ba16f80bcf34f6b8
Tyler Wymer <twymer@gmail.com>
2013-03-01 21:35:52 -0500

    Can change color and remove new colors

--- brick.js
+++ brick.js
@@ -1,6 +1,14 @@
+function brickIsPaddle (type) {
+  console.log(type);
+  return (type > 5 && type < 10);
+}
+
 function onBrickHit (ball) {
   if (ball.color() === this.color()) {
     this.destroy();
+  } else if (brickIsPaddle(this.type)) {
+    // shift type down to change to proper color
+    ball.color(brickColors[this.type - 4]);
   }
 }
 
@@ -8,7 +16,9 @@ Crafty.c('Brick', {
   init: function () {
   },
 
-  brick: function () {
+  brick: function (type) {
+    this.type = type;
+
     return this.attr({
       onHit: onBrickHit
     })
--- levels.js
+++ levels.js
@@ -4,12 +4,12 @@ function levelOne () {
     'C 0 0 0 0 4 4 0 0 0 0 C',
     'A 0 0 0 A 3 3 A 0 0 0 A',
     '0 0 0 B 3 3 3 3 B 0 0 0',
-    '0 8 B 2 2 2 2 2 2 B 8 0',
+    '0 8 B 2 2 2 2 2 2 B 6 0',
     '0 A 1 1 0 0 0 0 1 1 A 0',
     '0 0 0 0 0 B B 0 0 0 0 0',
     '0 0 0 0 0 B B 0 0 0 0 0',
     '0 A 1 1 0 0 0 0 1 1 A 0',
-    '0 8 B 3 3 3 3 3 3 B 8 0',
+    '0 7 B 3 3 3 3 3 3 B 9 0',
     '0 0 0 B 2 2 2 2 B 0 0 0',
     'A 0 0 0 A 2 2 A 0 0 0 A',
     'C 0 0 0 0 5 5 0 0 0 0 C']
@@ -29,7 +29,7 @@ function levelOne () {
         Crafty.e("Brick, 2D, Canvas, Color")
           .color(color)
           .attr({w: blockWidth-2, h: blockHeight-2, x: (blockWidth)*j+1, y: (blockHeight)*i+1})
-          .brick();
+          .brick(brickType);
       }
     }
   }
