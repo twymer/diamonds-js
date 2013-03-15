772f866131f327aad6fbe065614b718030ad3012
Tyler Wymer <twymer@gmail.com>
2013-03-15 11:14:06 -0400

    Added simple graphics for lock/key/reverse
    I need to finalize color scheme before these are correctly colored.
    They're supposed to be orange.

--- js/loading.js
+++ js/loading.js
@@ -20,12 +20,22 @@ function defineSprites() {
   Crafty.sprite(50, 30, "img/orangeBrush.png", {
     orangeBrush: [0, 0]
   });
+  Crafty.sprite(50, 30, "img/key.png", {
+    key: [0, 0]
+  });
+  Crafty.sprite(50, 30, "img/lock.png", {
+    lock: [0, 0]
+  });
+  Crafty.sprite(50, 30, "img/reverse.png", {
+    reverse: [0, 0]
+  });
 }
 
 Crafty.scene("loading", function () {
   Crafty.load(['img/diamond.png', 'img/ltblue.png', 'img/death.png',
                'img/blueBrush.png', 'img/brownBrush.png', 'img/pinkBrush.png',
-               'img/greenBrush.png', 'img/orangeBrush.png'], function () {
+               'img/greenBrush.png', 'img/orangeBrush.png',
+               'img/reverse.png', 'img/key.png', 'img/lock.png'], function () {
     Crafty.scene("game");
   });
 });
