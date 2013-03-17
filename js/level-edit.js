a2399096cafed2f1668dfbf8b0851bcd5347d635
Tyler Wymer <twymer@gmail.com>
2013-03-17 12:01:18 -0400

    Editor remembers level if you return to it

--- js/level-edit.js
+++ js/level-edit.js
@@ -3,14 +3,6 @@ Crafty.scene("edit", function () {
 
   window.gameBoard = [];
 
-  Crafty("Brick").destroy();
-
-  for (var i = 0; i < 12; i++) {
-    for (var j = 0; j < 12; j++) {
-      gameBoard[12 * j + i] = 0;
-    }
-  }
-
   // track whether or not the user is holding the mouse down so we can
   // let mouseHandler paint tiles
   // mouseIsDown can't be locally scoped or the editor breaks if user
@@ -43,59 +35,6 @@ Crafty.scene("edit", function () {
     }
   }
 
-  // populate board with gray bricks
-  for (var i = 0; i < 12; i++) {
-    for (var j = 0; j < 12; j++) {
-      Crafty.e("2D, Canvas, Color, Brick, Mouse")
-        .color(brickColors[0])
-        .attr({w: blockWidth-2, h: blockHeight-2,
-               x: (blockWidth)*j+1, y: (blockHeight)*i+1})
-        .brick(0, j, i)
-        .bind('MouseDown', mouseHandler)
-        .bind('MouseOver', mouseHandler);
-    }
-  }
-
-  // draw the block choices
-  for (var brickType = 0; brickType < Object.keys(brickTypes).length; brickType++) {
-    var typeName = brickTypeName(brickType);
-
-    color = brickColors[brickType];
-    Crafty.e("Brick, 2D, Canvas, Color, Mouse, Collision, " + typeName)
-      .color(color)
-      .attr({w: blockWidth - 2, h: blockHeight - 2,
-             x: blockHeight * (brickType + 1) * 1.05 + 1,
-             y: blockHeight * 12 + 1,
-             rotation: 90})
-      .brick(brickType)
-      .collision()
-      .bind('Click', function (e) {
-        activeType = this.type;
-        updateHighlight();
-      });
-  }
-
-  var updateHighlight = function () {
-    Crafty('Highlight').destroy();
-
-    // Crafty has trouble cleaning up custom canvas elements so
-    // I use a black screen to wipe those messy areas clean
-    var x = Crafty.e('2D, Canvas, Color')
-      .color("#000")
-      .attr({x: 0, y: blockHeight * 11,
-             w: Crafty.stage.elem.clientWidth, h: blockWidth * 3,
-             z: 100})
-
-    // draw a highlight around the active box
-    Crafty.e('2D, Canvas, Highlight')
-      .attr({x: blockHeight * (activeType) * 1.05 + 4,
-             h: blockWidth - 2, w: blockHeight,
-             y: blockHeight * 12, z: 15})
-      .Highlight()
-
-    x.destroy();
-  }
-
   Crafty.e("2D, DOM, Mouse, Text")
     .attr({x: Crafty.stage.elem.clientWidth - 53,
            y: blockHeight * 12.5,
@@ -117,6 +56,7 @@ Crafty.scene("edit", function () {
       }
     });
 
+  // Canvas element for custom drawing higlight on active block
   Crafty.c("Highlight", {
     Highlight: function () {
       return this;
@@ -145,4 +85,90 @@ Crafty.scene("edit", function () {
       ctx.stroke();
     }
   });
+
+  var updateHighlight = function () {
+    Crafty('Highlight').destroy();
+
+    // Crafty has trouble cleaning up custom canvas elements so
+    // I use a black screen to wipe those messy areas clean
+    var x = Crafty.e('2D, Canvas, Color')
+      .color("#000")
+      .attr({x: 0, y: blockHeight * 11,
+             w: Crafty.stage.elem.clientWidth, h: blockWidth * 3,
+             z: 100})
+
+    // draw a highlight around the active box
+    Crafty.e('2D, Canvas, Highlight')
+      .attr({x: blockHeight * (activeType) * 1.05 + 4,
+             h: blockWidth - 2, w: blockHeight,
+             y: blockHeight * 12, z: 15})
+      .Highlight()
+
+    x.destroy();
+  }
+
+  // Level edit scene code
+
+  // Clean up bricks left from state transition
+  Crafty("Brick").destroy();
+
+  var customLevel = localStorage['diamonds-game-custom-level'];
+  if (customLevel) {
+    window.gameBoard = customLevel.split(',');
+  } else {
+    // If we don't have a custom level, initialize empty board
+    for (var i = 0; i < 12; i++) {
+      for (var j = 0; j < 12; j++) {
+        gameBoard[12 * j + i] = 0;
+      }
+    }
+  }
+
+  // populate board with bricks
+  for (var i = 0; i < 12; i++) {
+    for (var j = 0; j < 12; j++) {
+      brickType = parseInt(gameBoard[12 * i + j]);
+      if (brickType === 0 || brickType === 35) {
+        Crafty.e("2D, Canvas, Color, Brick, Mouse")
+          .color(brickColors[0])
+          .attr({w: blockWidth-2, h: blockHeight-2,
+                 x: (blockWidth)*j+1, y: (blockHeight)*i+1})
+          .brick(0, j, i)
+          .bind('MouseDown', mouseHandler)
+          .bind('MouseOver', mouseHandler);
+      } else {
+        var typeName = brickTypeName(brickType);
+
+        var color = brickColors[brickType];
+        var brickAttrs = {w: blockWidth-2, h: blockHeight-2,
+                          x: (blockWidth) * j + 1, y: (blockHeight) * i + 1};
+
+        Crafty.e("Brick, 2D, Canvas, Color, Mouse, " + typeName)
+          .color(color)
+          .attr(brickAttrs)
+          .brick(brickType, j, i)
+          .bind('MouseDown', mouseHandler)
+          .bind('MouseOver', mouseHandler);
+      }
+    }
+  }
+
+  // draw the block choices
+  for (var brickType = 0; brickType < Object.keys(brickTypes).length; brickType++) {
+    var typeName = brickTypeName(brickType);
+
+    color = brickColors[brickType];
+    Crafty.e("Brick, 2D, Canvas, Color, Mouse, Collision, " + typeName)
+      .color(color)
+      .attr({w: blockWidth - 2, h: blockHeight - 2,
+             x: blockHeight * (brickType + 1) * 1.05 + 1,
+             y: blockHeight * 12 + 1,
+             rotation: 90})
+      .brick(brickType)
+      .collision()
+      .bind('Click', function (e) {
+        activeType = this.type;
+        updateHighlight();
+      });
+  }
 });
